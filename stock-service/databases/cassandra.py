ab8a29995fabad93ffe53c89e335e11d6562b7ba
vstam <just.van.stam@gmail.com>
2021-06-05 22:06:01 +0200

    add multi subtraction to cassandra stock service

--- stock-service/databases/cassandra.py
+++ stock-service/databases/cassandra.py
@@ -2,7 +2,9 @@ import logging
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 from cassandra.cqlengine.columns import Decimal
-from cassandra.policies import DCAwareRoundRobinPolicy
+from cassandra.query import BatchStatement, SimpleStatement, BatchType
+from cassandra.cqltypes import CounterColumnType
+from cassandra.query import dict_factory
 from uuid import uuid4, UUID
 
 LOGGER = logging.getLogger()
@@ -13,6 +15,7 @@ handler.setFormatter(logging.Formatter(
 LOGGER.addHandler(handler)
 
 KEYSPACE = "stock_service"
+
 
 # TODO: check if we can use async in some queries
 
@@ -26,7 +29,7 @@ class CassandraDatabase():
 
         auth = PlainTextAuthProvider(username="cassandra", password="password")
 
-        self.cluster = Cluster(['cassandra'], port=9042,
+        self.cluster = Cluster(['127.0.0.1'], port=9042,
                                protocol_version=3, auth_provider=auth)
         LOGGER.info("Connecting to cluster")
         self.connection = self.cluster.connect()
@@ -40,7 +43,6 @@ class CassandraDatabase():
                                 )
 
         self.connection.set_keyspace(KEYSPACE)
-
         LOGGER.info("Instantiating table stock-service")
         self.connection.execute("""CREATE TABLE IF NOT EXISTS stock (
                                    itemid uuid,
@@ -84,6 +86,23 @@ class CassandraDatabase():
             'price': item.one()[0],
         } if item.one() != None else None
 
+    def get_all(self):
+        item_counts = self.connection.execute(SimpleStatement("SELECT itemid FROM stock_service.stock"))
+        return item_counts.all()
+
+    def get_ids(self, ids):
+        query = '''
+        SELECT * 
+        FROM stock_service.stock_counts
+        WHERE itemid IN ({}) 
+        ALLOW FILTERING'''.format(','.join('%s' for _ in ids))
+        uuids = tuple(map(lambda x: UUID(x), ids))
+        results = self.connection.execute(query, uuids).all()
+        resultsDict = {}
+        for result in results:
+            resultsDict[str(result[0])] = result[1]
+        return resultsDict
+
     def add_item(self, itemid: UUID, number: int):
         """Add items to the stock"""
         item = self.get(itemid)
@@ -102,7 +121,7 @@ class CassandraDatabase():
         if item is None:
             return 404
         else:
-            if(item['stock'] < number):
+            if (item['stock'] < number):
                 LOGGER.info("input number is larger than the stock!")
                 return 400
             else:
@@ -110,3 +129,21 @@ class CassandraDatabase():
                                            SET quantity = quantity - %s
                                            WHERE itemid = %s
                                         """, (number, itemid))
+
+    def subtract_multiple(self, items: dict):
+        update_statement = "UPDATE stock_service.stock_counts SET quantity = quantity - %s  WHERE itemid = %s"
+        idWithQuanties = self.get_ids(list(items.keys()))
+
+        batch = BatchStatement(retry_policy=0, consistency_level=9, serial_consistency_level=1,
+                               batch_type=BatchType.COUNTER)
+        for item in items.keys():
+            if item not in idWithQuanties or idWithQuanties[item] - int(items[item]) < 0:
+                return 400
+            uuid = UUID(item)
+            batch.add(SimpleStatement(update_statement), (int(items[item]), uuid))
+
+        try:
+            self.connection.execute(batch)
+            return 201
+        except Exception as e:
+            return 400
